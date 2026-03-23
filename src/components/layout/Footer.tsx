07d00759ee4ba0722d87f6080ae37f92911095a0
samuel joly <s.joly@otaree.com>
2026-03-23 18:57:01 +0100

    Ad sponsors
    Faudrait faire des pubs style internet 2000 dans le theme pixel
    architect

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,6 +2,11 @@ export function Footer() {
   return (
     <footer>
       <p>Tenir la Ligne Zero. Corriger l'irreparable.</p>
+      <span>
+        <img src="public/images/cdw-big.gif" alt="necessary ad" />
+        <img src="public/images/barnes-and-noble-big.gif" alt="absolutely vital ad" />
+        <img src="public/images/cars.com.gif" alt="absolutely vital ad" />
+      </span>
     </footer>
   );
 }
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -11,27 +11,33 @@ export function Navbar() {
   };
 
   return (
-    <nav>
-      <Link to="/">Core Nexus</Link>
-      <ul>
-        <li>
-          <Link to="/sparks">Sparks</Link>
-        </li>
-        <li>
-          <Link to="/implants">Implants</Link>
-        </li>
-        <li>
-          <Link to="/architects">Architectes</Link>
-        </li>
-        <li>
-          <Link to="/sigils">Sigils</Link>
-        </li>
-        <li>
-          <button type="button" onClick={handleLogout}>
-            Logout
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <div className="nav">
+      <div>
+        <img src="public/images/economist-tall.gif" alt="required ad placement" />
+        <nav>
+          <Link to="/">Core Nexus</Link>
+          <ul>
+            <li>
+              <Link to="/sparks">Sparks</Link>
+            </li>
+            <li>
+              <Link to="/implants">Implants</Link>
+            </li>
+            <li>
+              <Link to="/architects">Architectes</Link>
+            </li>
+            <li>
+              <Link to="/sigils">Sigils</Link>
+            </li>
+            <li>
+              <button type="button" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <img src="public/images/scottrade-tall.gif" alt="necessary ad" />
+    </div>
   );
 }
